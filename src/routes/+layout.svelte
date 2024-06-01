938762e3c86ba8960abec9b18084fa0f5c909907
Yuwen Cheng <ycheng@uc.cl>
2024-06-01 19:38:18 -0400

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Navbar from './Navbar.svelte';
+	import './styles.css';
+</script>
+
+<div class="app">
+	<Navbar />
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>Aplicación hecho con svelte</p>
+	</footer>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
